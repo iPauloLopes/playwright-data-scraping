d6b319b945649290582024992d3f9851c09775ee
Paulo Lopes <133612870+iPauloLopes@users.noreply.github.com>
2026-07-20 15:13:54 -0300

    Create loop to open all products pages

--- main.py
+++ main.py
@@ -9,13 +9,20 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
 
     pageIndex = 1
-    
     page.goto(f"https://iba-world.com/cocktails/all-cocktails/page/{pageIndex}/")
 
-    page.get_by_role("button", name="Yes").click()
-    
-    time.sleep(5)
-    browser.close()
+    page.get_by_role("button", name="Yes").click() #bypass age verification
+
+
+    cocktailList = page.locator("#content > div > div.elementor.elementor-110 > section.elementor-section.elementor-top-section.elementor-element.elementor-element-5de9ae6.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div > div > div.iba-cocktails-container > div")
+    links = cocktailList.get_by_role("link").all()
+
+    for link in links:
+        url = link.get_attribute("href")
+        p = context.new_page()
+        p.goto(url)
+        time.sleep(2)
+        p.close()
 
 with sync_playwright() as playwright:
     run(playwright)
