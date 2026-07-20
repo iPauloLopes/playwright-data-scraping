b3f4b29287380c8ca63fb9295aa05cb779ad0733
Paulo Lopes <133612870+iPauloLopes@users.noreply.github.com>
2026-07-20 16:08:09 -0300

    Create pagination loop

--- main.py
+++ main.py
@@ -8,21 +8,34 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
 
-    pageIndex = 1
-    page.goto(f"https://iba-world.com/cocktails/all-cocktails/page/{pageIndex}/")
-
+    page.goto(f"https://iba-world.com/cocktails/all-cocktails/page/1/")
     page.get_by_role("button", name="Yes").click() #bypass age verification
+    
+    while True:
 
+        cocktailPage = page.locator("#content > div > div.elementor.elementor-110 > section.elementor-section.elementor-top-section.elementor-element.elementor-element-5de9ae6.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div > div > div.iba-cocktails-container > div")
+        cocktailList = cocktailPage.get_by_role("link").all()
 
-    cocktailList = page.locator("#content > div > div.elementor.elementor-110 > section.elementor-section.elementor-top-section.elementor-element.elementor-element-5de9ae6.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div > div > div.iba-cocktails-container > div")
-    links = cocktailList.get_by_role("link").all()
+        for link in cocktailList:
+            links = link.get_attribute("href")
 
-    for link in links:
-        url = link.get_attribute("href")
-        p = context.new_page()
-        p.goto(url)
-        time.sleep(2)
-        p.close()
+            p = context.new_page()
+            p.goto(links)
+
+#            time.sleep(1)
+
+            p.close()
+
+        nextPageButton = page.locator("#content > div > div.elementor.elementor-110 > section.elementor-section.elementor-top-section.elementor-element.elementor-element-5de9ae6.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default > div > div > div > div > div > div > div.iba-cocktails-pagination > a.next.page-numbers")
+
+        # breaks the loop if theres no 'next page' button
+        if nextPageButton.is_visible():
+            nextPageButton.click()
+        else:
+            break
+
+        page.wait_for_load_state()
+
 
 with sync_playwright() as playwright:
     run(playwright)
